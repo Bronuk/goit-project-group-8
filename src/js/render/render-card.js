ec51711961d56a8a1051d879c80dedd7588a2e50
IrynaMel <iryna.melnyk270692@gmail.com>
2022-02-09 22:52:27 +0200

    fixing button

--- src/js/render/render-card.js
+++ src/js/render/render-card.js
@@ -16,6 +16,8 @@ let movieId = ''
 function getGallery (e){
     e.preventDefault()
     movieId = e.target.dataset.id;
+   
+    
     
     if (e.target.nodeName !== 'IMG' 
     && e.target.nodeName !== 'H2' 
@@ -32,6 +34,8 @@ function getGallery (e){
             instance.show();
             // document.body.style.overflow = "hidden";           
             const closeBtn = document.querySelector('.modal__close-btn');
+
+            
       closeBtn.addEventListener('click', closeModal);
       window.addEventListener('keydown', closeModalHandler);
 
@@ -65,58 +69,102 @@ function getGallery (e){
 
 
 
+      let allQueue = JSON.parse(localStorage.getItem('queue'));
+      let allWatched = JSON.parse(localStorage.getItem('watched'));
+    
+   const addToQueue = document.querySelector('.modal__btn__white');
+   const addToWatched = document.querySelector('.modal__btn__orange');
+   
+   textButtonQ(movieId);
+   textButtonW(movieId)
 
+   function textButtonQ(movieId){
+      if (!allQueue.includes(movieId)){
+          addToQueue.textContent = 'ADD TO QUEUE';
+      } else{
+        addToQueue.textContent = 'REMOVE FROM QUEUE';
+      }}
 
-      const addToWatched = document.querySelector('.modal__btn__orange');
-      const addToQueue = document.querySelector('.modal__btn__white');
+      function textButtonW(movieId){
+        if (!allWatched.includes(movieId)){
+          addToWatched.textContent = 'ADD TO WATCHED';
+        } else{
+          addToWatched.textContent = 'REMOVE FROM WATCHED';
+        }}
+
+      
+      
       addToWatched.addEventListener('click', () => {
         addToWatched.style.backgroundColor = '#FF6B01'
         addToWatched.style.color = "white"
         addToWatched.style.border = 'none'
+        addToWatched.textContent = 'REMOVE FROM WATCHED';
 
         if (localStorage.getItem('watched') == null) {
           localStorage.setItem('watched', '[]');
 
         }
-        let allEntries = JSON.parse(localStorage.getItem('watched'));
-        if (!allEntries.includes(movieId)) {
-          allEntries.push(movieId);
-          localStorage.setItem('watched', JSON.stringify(allEntries));
+        
+        if (!allWatched.includes(movieId)) {
+          allWatched.push(movieId);
+          localStorage.setItem('watched', JSON.stringify(allWatched));
         }
+        else if(allWatched.includes(movieId)){
+          let index = allWatched.indexOf(movieId)
+          allWatched.splice(index, 1);
+          localStorage.setItem('queue', JSON.stringify(allWatched));
+          addToWatched.textContent = 'ADD TO WATCHED';
+          }
       });
+
+
+
       addToQueue.addEventListener('click', () => {
         addToQueue.style.backgroundColor = '#FF6B01'
         addToQueue.style.color = "white"
         addToQueue.style.border = 'none'
+        textButtonQ(movieId)
+        addToQueue.textContent = 'REMOVE FROM QUEUE';
+        
         if (localStorage.getItem('queue') == null) {
           localStorage.setItem('queue', '[]');
         }
-        let allEntries = JSON.parse(localStorage.getItem('queue'));
-        if (!allEntries.includes(movieId)) {
-          allEntries.push(movieId);
-          localStorage.setItem('queue', JSON.stringify(allEntries));
+        // let allEntries = JSON.parse(localStorage.getItem('queue'));
+        if (!allQueue.includes(movieId)) {
+          allQueue.push(movieId);
+          localStorage.setItem('queue', JSON.stringify(allQueue));
         }
+        else if(allQueue.includes(movieId)){
+          let index = allQueue.indexOf(movieId)
+          allQueue.splice(index, 1);
+          localStorage.setItem('queue', JSON.stringify(allQueue));
+          addToQueue.textContent = 'ADD TO QUEUE';
+          console.log(allQueue)}
+        
+        
+        
+
       });
       //  active or disable buttons
-      let allEntries = JSON.parse(localStorage.getItem('watched'));
+      // let allEntries = JSON.parse(localStorage.getItem('watched'));
 
-      if (allEntries.includes(movieId)) {
-        addToWatched.style.backgroundColor = '#FF6B01'
-        addToWatched.style.color = "white"
-        addToWatched.style.border = 'none'
-        addToWatched.disabled = true;
+      // if (allEntries.includes(movieId)) {
+      //   addToWatched.style.backgroundColor = '#FF6B01'
+      //   addToWatched.style.color = "white"
+      //   addToWatched.style.border = 'none'
+      //   addToWatched.disabled = true;
 
-      }
+      // }
 
-      let allEntrie = JSON.parse(localStorage.getItem('queue'))
+      // let allEntrie = JSON.parse(localStorage.getItem('queue'))
 
-      if (allEntrie.includes(movieId)) {
-        addToQueue.style.backgroundColor = '#FF6B01'
-        addToQueue.style.color = "white"
-        addToQueue.style.border = 'none'
-        addToQueue.disabled = true;
+      // if (allEntrie.includes(movieId)) {
+      //   addToQueue.style.backgroundColor = '#FF6B01'
+      //   addToQueue.style.color = "white"
+      //   addToQueue.style.border = 'none'
+        
 
-      }
+      // }
     })
   } catch (error) {
     console.log(error);
